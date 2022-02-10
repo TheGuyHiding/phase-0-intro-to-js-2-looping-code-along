c405d4e4891e15bd8e27a879b4f0b50156668342
Luis <luis9609@gmail.com>
2022-02-10 00:20:43 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,20 @@
 // Code your solutions in this file
+const cards = ["Guadalupe", "Ollie", "Aki"];
+
+function writeCards(cards){
+    for (let i = 0; i < cards.length; i++){
+        console.log(`Thank you, ${cards[i]}, for the wonderful suprise gifts!`);
+    }
+
+    return cards;
+}
+
+writeCards(cards);
+
+let integer = 10;
+
+function countDown(integer){
+    while (integer >= 0){
+        console.log(integer--);
+    }
+}
